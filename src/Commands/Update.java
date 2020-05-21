7ab8a1e16628153be95ba4b322cbdf42377bc7a8
dI00 <65408739+dI00@users.noreply.github.com>
2020-05-21 08:36:17 +0300

    Update Update.java

--- src/Commands/Update.java
+++ src/Commands/Update.java
@@ -2,28 +2,28 @@ package Commands;
 
 import Controller.CommandWithObject;
 import Controller.DragonCollection;
-import Dragon.Dragon;
-import Dragon.Dragonborn;
+import  Organization. Organization;
+import Organization. Organizator;
 
 import java.util.InputMismatchException;
 
 public class Update implements CommandWithObject {
     String name = "update";
-    Dragonborn creater = new Dragonborn();
-    DragonCollection collection = new DragonCollection();
+    Organizator creater = new  Organizator();
+    OrganizationCollection collection = new OrganizationCollection();
 
     @Override
     public void execute(Object arg) {
         try {
             int id = Integer.parseInt((String) arg);
             if (this.check(id)) {
-                Dragon dragon = this.getNewDragon(null);
-                if (dragon != null) {
-                    dragon.setId(id);
-                    collection.update((Integer) id, dragon);
+                Organization organization = this.getNewOrganization(null);
+                if (organization != null) {
+                    organization.setId(id);
+                    collection.update((Integer) id, organization);
                     System.out.println("Дракон с id[" + arg + "] успешно обновлен.");
                 }
-            } else System.out.println("Дракон с указанным id не найден.");
+            } else System.out.println("Organization с указанным id не найден.");
         } catch (NumberFormatException | InputMismatchException e) {
             System.out.println("Аргумент команды должен быть типа \"int\"");
         }
@@ -43,9 +43,9 @@ public class Update implements CommandWithObject {
     }
 
     @Override
-    public Dragon getNewDragon(Object params) {
-        if (Dragonborn.isFromScript == false) return creater.create();
-        else return Dragonborn.dragonFromScript;
+    public Organization getNewOrganization(Object params) {
+        if (Organizator.isFromScript == false) return creater.create();
+        else return Organizator.organizationFromScript;
     }
 }
 
